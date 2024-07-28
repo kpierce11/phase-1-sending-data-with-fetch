a6a218df252394f471bbde872ab5455b9702970f
Keira Pierce <kpierce1@samford.edu>
2024-07-28 03:53:00 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Add your code here
+function submitData(name, email) {
+    const configurationObject = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        name: name,
+        email: email
+      })
+    };
+  
+    return fetch('http://localhost:3000/users', configurationObject)
+      .then(response => response.json())
+      .then(data => {
+        const idElement = document.createElement('p');
+        idElement.textContent = data.id;
+        document.body.appendChild(idElement);
+      })
+      .catch((error) => {
+        const errorElement = document.createElement('p');
+        errorElement.textContent = error.message;
+        document.body.appendChild(errorElement);
+      });
+  }
+
